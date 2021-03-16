d8dfca13bed2c2915ff6ef4f636174744db23582
RobertsOrleans <robertsorleans@gmail.com>
2021-03-16 14:32:11 +0200

    6th lesson

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
-var titles = document.getElementsByClassName('title');
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
 
-///console.log(Array.isArray(titles));
-///console.log(Array.isArray(Array.from(titles)));
+console.log(search, bookList);
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
+const titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
+});
+
+Array.from(books).forEach(function(book){
+  book.textContent += ' (Book title)';
+});
+
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
+
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
+console.log('book list parent element:', bookList.parentElement);
+console.log('book list parent node:', bookList.parentNode);
+
+console.log('all node children:');
+Array.from(bookList.childNodes).forEach(function(node){
+  console.log(node);
+});
+
+console.log('all element children:');
+Array.from(bookList.children).forEach(function(node){
+  console.log(node);
+});
+
+console.log('Book titles:');
+Array.from(titles).forEach(function(title){
+  console.log(title.textContent);
+});
