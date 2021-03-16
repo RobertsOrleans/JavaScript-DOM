ec2b00ab0b338c91c7e710a909581d4ba520a117
RobertsOrleans <robertsorleans@gmail.com>
2021-03-16 14:34:16 +0200

    8th lesson

--- app.js
+++ app.js
@@ -58,3 +58,10 @@ console.log('Book titles:');
 Array.from(titles).forEach(function(title){
   console.log(title.textContent);
 });
+
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
